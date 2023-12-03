57b37ea5135856d61120e0dd163b342501e40da5
kwesi_amissah <150988868+PhilipAmissah@users.noreply.github.com>
2023-12-03 10:24:27 +0000

    Create ProjectsPage.js

---
+++ my-app/client/src/components/ProjectsPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ProjectsPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Projects</h1>
+                <p>Welcome to our projects page. Here you can find information about all our ongoing and completed projects.</p>
+            </div>
+        );
+    }
+}
+
+export default ProjectsPage;
