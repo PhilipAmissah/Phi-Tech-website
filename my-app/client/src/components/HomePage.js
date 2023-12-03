1701c4d7db6a84fc6029bb7b52d75fa9e46f7da2
kwesi_amissah <150988868+PhilipAmissah@users.noreply.github.com>
2023-12-03 10:23:51 +0000

    Create HomePage.js

---
+++ my-app/client/src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to our website!</h1>
+                <p>This is the homepage of our site. Feel free to browse around and learn more about us.</p>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
