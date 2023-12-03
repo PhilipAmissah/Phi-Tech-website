feca472f9ecf856e21fd1cf6f776876c3af8f7dc
kwesi_amissah <150988868+PhilipAmissah@users.noreply.github.com>
2023-12-03 10:19:50 +0000

    Create BlogPage.js

---
+++ my-app/client/src/components/BlogPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class BlogPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Blog</h1>
+        <p>Welcome to our blog page. Stay tuned for updates!</p>
+      </div>
+    );
+  }
+}
+
+export default BlogPage;
