e13e2e3222c91e9f384d7528891532e8cb4419f3
kwesi_amissah <150988868+PhilipAmissah@users.noreply.github.com>
2023-12-03 10:19:22 +0000

    Create AboutUsPage.js

---
+++ my-app/client/src/components/AboutUsPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutUsPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>About Us</h1>
+                <p>This is the About Us page.</p>
+            </div>
+        );
+    }
+}
+
+export default AboutUsPage;
