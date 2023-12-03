23f1dc99bb23ebf1dec34abbd7fc1a6e7008c25a
kwesi_amissah <150988868+PhilipAmissah@users.noreply.github.com>
2023-12-03 10:24:56 +0000

    Create ServicesPage.js

---
+++ my-app/client/src/components/ServicesPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ServicesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Services</h1>
+        <p>This is the Services page.</p>
+      </div>
+    );
+  }
+}
+
+export default ServicesPage;
